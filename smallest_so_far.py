a4fd035da6f1ea44f7a302fef66ae673087f09cb
Taranum <taranumashfaq1@gmail.com>
2026-07-23 20:06:17 +0500

    Implement logic to find the smallest number

---
+++ smallest_so_far.py
@@ -0,0 +1,7 @@
+smallest_so_far = -1
+ print('Before', smallest_so_far)
+ for the_num in [9,12,41,3,74,15]:
+     if the_num < smallest_so_far :
+         smallest_so_far = the_num
+     print(the_num, smallest_so_far)
+ print('After', smallest_so_far)
